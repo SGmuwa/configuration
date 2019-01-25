964ad266fc1d60a8ec8783930d3319a4665c8636
SGmuwa <motherlode.muwa@gmail.com>
2019-01-25 14:38:24 +0300

    Быстрый пример логирования в файл

--- configuration-client/src/main/java/demo/ProjectNameRestController.java
+++ configuration-client/src/main/java/demo/ProjectNameRestController.java
@@ -1,26 +1,34 @@
 package demo;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// <1>
 @RestController
 @RefreshScope
 class ProjectNameRestController {
 
- private final String projectName;
+ /**
+  * Создание лога. Логи можно создовать везде.
+  * Я создал в контроллере.
+  */
+ private Log log = LogFactory.getLog(getClass());
 
- @Autowired
- public ProjectNameRestController(
-  @Value("${configuration.projectName}") String pn) { // <2>
-  this.projectName = pn;
- }
+ /**
+  * Поле хранит в себе имя проекта.
+  */
+ private final String projectName = "My project";
 
+ /**
+  * Обработка запроса по url: /project-name
+  */
  @RequestMapping("/project-name")
  String projectName() {
+  log.info("get projectName: " + projectName);
   return this.projectName;
  }
 }
